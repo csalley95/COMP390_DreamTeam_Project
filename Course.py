e11ca3e7b6875a87f8b4f0432e0db0a0c58c6ef1
IanW-99 <66699279+IanW-99@users.noreply.github.com>
2021-11-13 18:58:09 -0500

    Basic database connection methods in main.py and test code in Course.py

--- Course.py
+++ Course.py
@@ -1,10 +1,19 @@
+import sqlite3 as sql
+from typing import Tuple
+import pandas as pd
+import sys
+
+
 class Course:
 
-    def __init__(self, courseID, courseName, credits):
-        self.courseID = courseID
-        self.courseName = courseName
-        self.credits = credits
+    def __init__(self, CourseID, Course_Credits, Course_Description):
+        self.Course_ID = CourseID
+        self.Course_Credits = Course_Credits
+        self.Course_Description = Course_Description
 
-    #def addCourse(self):
+    def addCourse(self, conn: sql.Connection, curs: sql.Cursor):
+        curs.execute("""INSERT INTO Course (CourseID, Course_Credits, Course_Description)
+                        VALUES (?,?,?)""", (self.Course_ID, self.Course_Credits, self.Course_Description))
+        conn.commit()
 
     #def editCourse(self):
--- main.py
+++ main.py
@@ -1,12 +1,38 @@
+import sqlite3 as sql
+from typing import Tuple
+import pandas as pd
+import sys
+
 from Student import Student
 from Instructor import Instructor
 from Course import Course
 from Section import Section
 from Enrollment import Enrollment
 
+
+
+
 def main():
-    print("Hello")
+    conn, curs = open_db('NorthStarProject.db')
+
+    test_select(curs, """SELECT * FROM Course""")
+
+    input('Press Enter to close program')  # Temporary way of keeping this open until we create gui
+    close_db(conn)
 
 
+def open_db(filename: str) -> Tuple[sql.Connection, sql.Cursor]:
+    db_connection = sql.connect(filename)
+    cursor = db_connection.cursor()
+    return db_connection, cursor
+
+def close_db(connection: sql.Connection):
+    connection.close()
+
+
+def test_select(curs: sql.Cursor, testString: str):  # Use this to test select statements
+    curs.execute(testString)
+    data = curs.fetchall()
+    print(data)
 
 main()
