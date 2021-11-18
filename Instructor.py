cf080c549aeff73d228513a7e3ea55a6b2ef03d1
csalley95 <c1salley@student.bridgew.edu>
2021-11-18 14:46:36 -0500

    Added methods to Inrstuctor class, will need to test

--- Instructor.py
+++ Instructor.py
@@ -4,7 +4,16 @@ class Instructor:
     def __init__(self, instructorID, instructorName, conn: sql.Connection, curs: sql.Cursor):
         self.instructorID = instructorID
         self.instructorName = instructorName
+        self.conn = conn
+        self.curs = curs
 
-    #def addInstructor(self):
+    def addInstructor(self):
+        self.curs.execute("""INSERT INTO Instructor (instructorID, instructorName)
+                                VALUES (?,?)""", (self.instructorID, self.instructorName))
+        self.conn.commit()
 
-    #def removeInstructor(self):
+    def removeInstructor(self):
+        sql_update_query = """Update Instructor set instructorID = ?, set instructorName = ? WHERE instuctorID = ?"""
+        data = (self.instructorID, self.instructorName, self.instructorID)
+        self.curs.execute(sql_update_query, data)
+        self.conn.commit()
