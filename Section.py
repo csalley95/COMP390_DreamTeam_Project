62a3db12712758d492aad77974aaeb3c6ed1453e
gbpar <gbpark1203@gmail.com>
2021-11-19 16:53:20 -0500

    First commit

--- Section.py
+++ Section.py
@@ -2,14 +2,32 @@ import sqlite3 as sql
 
 class Section:
 
-    def __init__(self, sectionID, conn: sql.Connection, curs: sql.Cursor):
+    def __init__(self, sectionID, instructor, conn: sql.Connection, curs: sql.Cursor):
         self.sectionID = sectionID
+        self.instructor = instructor
+        self.conn = conn
+        self.curs = curs
 
-    #def addInstructorToSection(self, instructor):
 
-    #def removeInstructorFromSection(self, instructor):
+    def addInstructorToSection(self, instructor):
+        CHECK_instructorID = self.curs.execute("""SELECT instructorID FROM instructor WHERE instructorID = ? """), (self.instructorID)
+        CHECK_instructorName = self.curs.execute("""SELECT instructorName FROM instructor WHERE instructorNameID = ? """), (self.instructorName)
 
-    #def addSection(self):
 
-    #def removeSection(self):
+    def removeInstructorFromSection(self, instructor):
 
+        self.curs.execute("""DELETE FROM section WHERE instructorID = ?, instructorName = ? """),\
+                                                                         (self.instructorID, self.instructorName)
+
+        self.conn.commit()
+
+    def addSection(self):
+        self.curs.execute("""INSERT INTO Section (sectionID, instructor)
+                               VALUES (?,?)""", (self.sectionID, self.instructor))
+        self.conn.commit()
+
+    def removeSection(self):
+        sql_update_query = """DELETE FROM Section WHERE SectionID = ?"""
+        data = (self.sectionID)
+        self.curs.execute(sql_update_query, data)
+        self.conn.commit()
