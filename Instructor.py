d9eaa30ebac8e4e061fa5bfca973870133b9636b
csalley95 <c1salley@student.bridgew.edu>
2021-11-21 20:50:32 -0500

    Added error checking to student and partially to instructor. Still need to test

--- Instructor.py
+++ Instructor.py
@@ -1,16 +1,21 @@
 import sqlite3 as sql
+
+
 class Instructor:
 
     def __init__(self, instructorID, instructorName, conn: sql.Connection, curs: sql.Cursor):
-        self.instructorID = instructorID
+        self.instructor_ID = instructorID
         self.instructorName = instructorName
         self.conn = conn
         self.curs = curs
 
     def addInstructor(self):
-        self.curs.execute("""INSERT INTO Instructor (instructorID, instructorName)
+        instructorID_form, instructorID_exists = self.valid_instructorID()
+        if instructorID_form == 0 and instructorID_exists == 0:
+            self.curs.execute("""INSERT INTO Instructor (instructorID, instructorName)
                                 VALUES (?,?)""", (self.instructorID, self.instructorName))
-        self.conn.commit()
+            self.conn.commit()
+        return instructorID_form, instructorID_exists
 
     def removeInstructor(self):
         sql_update_query = """DELETE FROM Instructor WHERE instuctorID = ?"""
--- Student.py
+++ Student.py
@@ -1,16 +1,44 @@
 import sqlite3 as sql
+
+
 class Student:
 
     def __init__(self, studentID, studentName, conn: sql.Connection, curs: sql.Cursor):
-        self.studentID = studentID
+        self.student_ID = studentID
         self.studentName = studentName
         self.conn = conn
         self.curs = curs
 
     def addStudent(self):
-        self.curs.execute("""INSERT INTO Student (studentID, studentName)
+        studentID_form, studentID_exists = self.valid_studentID()
+        if studentID_form == 0 and studentID_exists == 0:
+            self.curs.execute("""INSERT INTO Student (studentID, studentName)
                                         VALUES (?,?)""", (self.studentID, self.studentName))
-        self.conn.commit()
+            self.conn.commit()
+        return studentID_form, studentID_exists
+
+    def valid_studentID(self):
+        self.studentID_form = 0
+        self.studentID_exists = 0
+
+        for i in range(3):
+            if not self.student_ID[i].isalpha():
+                self.studentID_form = 1
+                break
+        if self.studentID_form == 0:
+            for i in range(4, 6):
+                if not self.student_ID[i].isdigit():
+                    self.studentID_form = 1
+                    break
+
+        check_exists_query = """SELECT EXISTS(SELECT 1 FROM Student WHERE studentID = ?) """
+        data = self.student_ID,
+        self.curs.execute(check_exists_query, data)
+        # only commits if course doesnt already exist
+        if self.curs.fetchone()[0] == 1:
+            self.studentID_exists = 1
+
+        return self.studentID_form, self.studentID_exists
 
     def removeStudent(self):
         sql_update_query = """DELETE FROM Student WHERE studentID = ?"""
