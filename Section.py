9a5d68c9eb0e6d4817d6f974a3fe576837dabf7c
gbpar <gbpark1203@gmail.com>
2021-11-23 13:20:43 -0500

    fixing remove Instructor from section

--- Section.py
+++ Section.py
@@ -11,30 +11,29 @@ class Section:
     def addInstructorToSection(self):
         CHECK_instructorID = self.curs.execute("""SELECT instructorID FROM instructor WHERE instructorID = ? """), (self.instructorID)
         CHECK_sectionID = self.curs.execute("""SELECT sectionID FROM Section WHERE sectionID = ? """), (self.sectionID)
-        CHECK_courseID = self.curs.execute("""SELECT courseID FROM Course WHERE courseID = ? """), (self.courseID)
 
-        if (CHECK_instructorID != False) and (CHECK_sectionID != False) and (CHECK_courseID != False):
-            self.curs.execute("""INSERT INTO Section (instructorID, sectionID, courseID)
-                                                             VALUES (?,?,?)""", (self.studentID, self.sectionID, self.courseID))
+        if (CHECK_instructorID != False) and (CHECK_sectionID != False):
+            self.curs.execute("""INSERT INTO Section (instructorID, sectionID)
+                                                             VALUES (?,?,?)""", (self.studentID, self.sectionID))
             self.conn.commit()
 
     def removeInstructorFromSection(self):
 
-        self.curs.execute("""DELETE FROM Section WHERE instructorID = ?, sectionID = ? , courseID = ? """),\
-                                                                         (self.insturctorID, self.sectionID, self.courseID)
-
+        self.curs.execute("""DELETE FROM Section WHERE instructorID = ?, sectionID = ? """),\
+                                                                         (self.insturctorID, self.sectionID)
         self.conn.commit()
 
     def addSection(self):
         sectionID_form, sectionID_exists = self.valid_sectionID()
         if sectionID_form == 0 and sectionID_exists == 0:
-            self.curs.execute("""INSERT INTO Section (sectionID, instructorName)
-                                VALUES (?,?)""", (self.sectionID, self.instructorName))
+            self.curs.execute("""INSERT INTO Section (SectionID, Instructor_Name)
+                                VALUES (?,?)""", (self.sectionID, self.instructor_Name))
             self.conn.commit()
-        return sectionID_form  #,instructorName_exists
+        return sectionID_form, sectionID_exists
 
     def removeSection(self):
         sql_update_query = """DELETE FROM Section WHERE sectionID = ?"""
-        data = (self.sectionID)
+        data = self.sectionID,
         self.curs.execute(sql_update_query, data)
         self.conn.commit()
+
